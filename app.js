2b4eff23d8e5bef56e9ad5a30ae8a0768098f43f
piotr-t <troc.piotr@gmail.com>
2019-03-19 16:03:27 +0100

    no message

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const inquirer = require("inquirer");
+//1)
 // scope from 2 to max
 let max = 30;
 
@@ -45,8 +47,29 @@ let { getPrimNumbers } = new GetPrimaryNumbers(getNumbers);
 console.log(...getNumbers);
 console.log('liczby pierwsze:', ...getPrimNumbers);
 
+//2)
+class GuessingGame extends GetRandomScope {
 
-// fibonacci
+    constructor(max) {
+        let { rand } = super(max);
+        this.rand = rand;
+    }
+}
+let Guess = new GuessingGame(1000);
+console.log("2)", Guess.rand);
+//3)rotate list
+class RotateList {
+    constructor(k, list) {
+        this.k = k - Math.floor(k / list.length) * list.length
+        this.list = list.slice(this.k).concat(list.slice(0, this.k));
+    }
+}
+let list = [1, 2, 3, 4, 5];
+let RL = new RotateList(102, list);
+console.log(RL.list);
+
+
+//4) fibonacci
 
 let scopeFibonnacci = 10;
 let randFibonacci = new GetRandomScope(scopeFibonnacci).rand;
@@ -71,6 +94,46 @@ console.log('randFibonacci', randFibonacci);
 console.log(...fib);
 console.log(...fib1);
 
+//5) 
 
+class TArray {
+    constructor(wordBefore) {
+        wordBefore = wordBefore + "";
+        let wordAfter = [];
+        for (let i = 0; i < wordBefore.length; i++) {
+            wordAfter.push(wordBefore[i]);
+        }
+        return wordAfter;
+    }
+}
+
+let nToArray = new TArray(12345678);
+console.log(nToArray);
+
+
+//6) PigLatin
+
+class PigLatin {
+    constructor(textBefore) {
+        return textBefore.split(" ").map(item => {
+            let i0 = item[0].toLowerCase();
+            if (item[item.length - 1] !== ',') {
+                if (item[0].toLowerCase() !== item[item.length - 1].toLowerCase()) {
+                    item += i0;
+                }
+                item += "ay";
+            } else if (item.length > 1) {
+                let iEnd = item[item.length - 1];
+                item = item.slice(0, item.length - 1);
+                item += i0;
+                item += "ay" + iEnd;
+            }
+            return item.slice(1);
+        });
+    }
+}
+
+let textAfter = new PigLatin("Litwo ojczyzno moja, ty jesteś jak zdrowie");
+console.log(...textAfter);
 
 
